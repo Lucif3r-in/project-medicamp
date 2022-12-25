baeb85cfb0df3ba1dd0224cb09d5c9555e8bb549
Debaditya <ddev2636@gmail.com>
2022-12-25 17:00:45 +0530

    timeline

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+
 import "./App.css";
+import Footer from "./component/Footer/Footer";
 import HeroSection from "./component/HeroSection/HeroSection";
+import Navbar from "./component/Navbar/Navbar"
 import Slider from "./component/Reviews/Slider";
 import Surgery from "./component/Surgery/Surgery";
 import Form from "./component/form/Form";
@@ -8,6 +11,7 @@ import BasicTimeline from "./component/timeline/Timeline";
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <HeroSection />
       <div className="flex items-center justify-around">
       <Surgery />
@@ -17,6 +21,7 @@ function App() {
       <div className="mt-10 ml-5">
       <BasicTimeline/>
       </div>
+      <Footer />
     </div>
   );
 }
